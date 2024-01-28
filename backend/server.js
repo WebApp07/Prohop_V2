4cd0fe251ce33e982e9ce8d80ec68dab6c4b96cb
unknown <aminezouguig@proton.me>
2024-01-28 03:19:49 +0100

    Fetch Products

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import products from "./data/products.js";
+
 const port = process.env.PORT || 5000;
 
 
@@ -11,7 +12,7 @@ app.get("/", (req, res) => {
     res.send("API is running...");
 });
 
-app.get("/products", (req, res) => {
+app.get("/api/products", (req, res) => {
     res.json(products);
 });
 
