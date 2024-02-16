7b23ba54696381bb43deb00dcc0549b720042e21
WepApp07 <AmineZouguig@proton.me>
2024-02-16 02:59:02 -0800

    User email and password validation

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,11 @@ import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 connectDB();
 
 app.get("/", (req, res) => {
