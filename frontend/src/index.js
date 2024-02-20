b99ca7991af6d513fe7de6b3c6da3c92327d04d8
WepApp07 <AmineZouguig@proton.me>
2024-02-20 07:33:10 -0800

    Login Screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,38 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import {
   createBrowserRouter,
   CreateRouteesFromElements,
   createRoutesFromElements,
   Route,
-  RouterProvider
+  RouterProvider,
 } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./store"
-import './index.css';
+import store from "./store";
+import "./index.css";
 // import "bootstrap/dist/css/bootstrap.min.css";
-import './assets/styles/bootstrap.custom.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
-import CartScreen from './screens/CartScreen';
+import "./assets/styles/bootstrap.custom.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-   <Route path='/' element={<App />}>
-    <Route index={true} path='/' element={<HomeScreen />} />
-    <Route path='/product/:id' element={<ProductScreen />} />
-    <Route path='/cart' element={<CartScreen />} />
-   </Route>
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
+      <Route path="/cart" element={<CartScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
+    </Route>
   )
 );
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
