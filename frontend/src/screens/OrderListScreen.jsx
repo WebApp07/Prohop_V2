76c3e87d7d4ea15006894027090894ba21cbd96f
WebApp07 <aminezouguig@proton.me>
2024-04-02 10:10:09 -0700

    List order for admin

--- frontend/src/screens/OrderListScreen.jsx
+++ frontend/src/screens/OrderListScreen.jsx
@@ -1,7 +1,69 @@
-import React from "react";
+import { Button, Table } from "react-bootstrap";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import { useGetOrdersQuery } from "../slices/ordersapiSlice";
+import { FaTimes } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
 
 const OrderListScreen = () => {
-  return <div>OrderListScreen</div>;
+  const { data: orders, isLoading, error } = useGetOrdersQuery();
+
+  return (
+    <>
+      <h1>Orders</h1>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Table striped hover responsive className="table-sm">
+          <thead>
+            <tr>
+              <td>ID</td>
+              <td>USER</td>
+              <td>DATE</td>
+              <td>TOTAL</td>
+              <td>PAID</td>
+              <td>DELIVERED</td>
+              <td></td>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order) => (
+              <tr key={order._id}>
+                <td>{order._id}</td>
+                <td>{order.user && order.user.name}</td>
+                <td>{order.createdAt.substring(0, 10)}</td>
+                <td>${order.totalPrice}</td>
+                <td>
+                  {order.isPaid ? (
+                    order.paidAt.substring(0, 10)
+                  ) : (
+                    <FaTimes style={{ color: "red" }} />
+                  )}
+                </td>
+
+                <td>
+                  {order.isdelivered ? (
+                    order.deliveredAt.substring(0, 10)
+                  ) : (
+                    <FaTimes style={{ color: "red" }} />
+                  )}
+                </td>
+                <td>
+                  <LinkContainer to={`/order/${order._id}`}>
+                    <Button variant="light" className="btn-sm">
+                      Details
+                    </Button>
+                  </LinkContainer>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </>
+  );
 };
 
 export default OrderListScreen;
