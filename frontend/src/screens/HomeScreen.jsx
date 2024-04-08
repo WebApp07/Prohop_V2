497f177a2264b7c138118554581940430192c289
WebApp07 <aminezouguig@proton.me>
2024-04-08 09:50:32 -0700

    Pagination Products

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,35 +1,34 @@
-import Product from "../components/Product"
-import { Row, Col, Spinner } from "react-bootstrap"
-import Loader from "../components/Loader"
-import Message from "../components/Message"
-import { useGetProductsQuery } from "../slices/productApiSlice"
+import Product from "../components/Product";
+import { useParams } from "react-router-dom";
+import { Row, Col, Spinner } from "react-bootstrap";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import { useGetProductsQuery } from "../slices/productApiSlice";
 
 const HomeScreen = () => {
-
-  const { data:  products, isLoading, error } = useGetProductsQuery();
+  const { pageNumber } = useParams();
+  const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
 
   return (
     <>
-
-    {isLoading ? (
-      <Loader />
-    ) : error ? (<Message variant='danger'>{error.data?.message || error.error}</Message>) : (<>
-       <h1>Latest Products</h1>
-       <Row>
-           {products.map((product) => (
-               <Col key={product._id} sm={12} md={10} lg={4} xl={3}>
-                  <Product product={product} /> 
-               </Col>
-           ))}
-       </Row>
-    
-    </>) }
-
-
-
-     
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error.data?.message || error.error}</Message>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
+          <Row>
+            {data.products.map((product) => (
+              <Col key={product._id} sm={12} md={10} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
